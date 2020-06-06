7badf1c0546c95797be533a3df93c80891d1fea2
kibirio <ruiruharrison047@gmail.com>
2020-06-06 17:15:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+// Smooth scrolling
+$('.cf a').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            600,
+            function() {
+                window.location.hash = hash;
+            }
+        );
+    }
+});
